3e09a1cfb78ce901a86743e647c5569cd015f7b9
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2023-06-18 05:55:57 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Aegis",
+    version = "0.0.1",
+    author = "Govind Gnanakumar",
+    author_email = "govind@automorphic.ai",
+    description = ("Aegis is a self-hardening firewall for large language models."
+                   " Use it to protect your models from adversarial attacks:"
+                   " prompt injections, prompt leakage, PII detection, and more."),
+    license = "MIT",
+    keywords = "llm, language model, security, adversarial attacks, prompt injection, prompt leakage, PII detection, self-hardening, firewall",
+    # url = "http://packages.python.org/an_example_pypi_project",
+    packages=['aegis'],
+    long_description=read('README.md'),
+)
